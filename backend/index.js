074b5fae70827f1521298ce317937e55d81137bb
Max Belohoubek <max.belohoubek@gob.de>
2019-07-11 16:16:06 +0200

    yes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require("body-parser");
+const fs = require('fs');
 var cors = require('cors');
 const app = express();
+
+const { settings } = require('./settings');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -16,6 +19,11 @@ app.get('/', (req, res) => {
 app.post('/create', (req, res) => {
   res.status(200).json(req.body);
   
+  fs.writeFile(settings.localPath,JSON.stringify(req.body,null,2), (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
 });
 
 const PORT = process.env.PORT || 5001;
